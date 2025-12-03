6c025e3e6e17a047bf056e60c5690c617ef75ffd
Roberto Rossi <robros@gmail.com>
2025-12-03 02:12:17 +0000

    feat: improve display by hiding cursor, optimizing screen clearing, and using printf for terminal control.

--- watch_kanban.sh
+++ watch_kanban.sh
@@ -1,6 +1,16 @@
-#!/bin/bash
+
+#!/usr/bin/env bash
+
+printf '\e[?25l'           # hide cursor
+trap 'printf "\e[?25h"' EXIT
+
+# Do a full clear only once
+printf '\e[2J\e[H'
+
 while true; do
-    tput cup 0 0   # Move cursor to row 0, column 0
+    # Go home, clear only below current cursor to end of screen
+    printf '\e[H'
     todo kanban pending in-progress completed
+    printf '\e[J'
     sleep 1
 done
